d415206621dc3d2d6c899dc967763ef0ea12026e
Duc <voleduc@gmail.com>
2025-10-16 12:08:06 +0000

    xong duyet

--- src/week_5/Binary tree.cpp
+++ src/week_5/Binary tree.cpp
@@ -5,7 +5,6 @@ template <class Item>
 struct Node
 {
 Item data; // Dữ liệu chứa trong mỗi đỉnh
-Node* root;
 Node* left;
 Node* right;
 };
@@ -13,18 +12,26 @@ Node* right;
 template <class Item>
 void Preorder (Node <Item> * root, void f (Item &)){
 // Thăm các đỉnh của cây nhị phân theo thứ tự trước
-if (root! = NULL){
-    f (root ->data);
+if (root!= NULL){
+    f (root ->data)
     Preorder (root ->left, f);
-    Preorder (root ->right, f);
+    Preorder (root ->right, f); 
   }  
 }
 template <class Item>
 void Inorder(Node <Item> * root, void f (Item &)){
-
+  if (root!= NULL){
+   Inorder (left ->root, f);
+  f (root ->data);
+  Inorder (right ->root, f);
+  }
 }
 
 template <class Item>
 void Postorder(Node <Item> * root, void f (Item &)){
-
+  if (root!= NULL){
+  Postorder (left ->root, f);
+  Postorder (right ->root, f);
+  f (root-> data);
+  }
 }
